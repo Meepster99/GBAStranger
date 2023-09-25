d77c0054631497f0db6b7f69cabcb449230be9c2
Venus Xeon-Blonde <venusflameblonde@gmail.com>
2023-09-25 01:51:55 -0400

    Add build script

---
+++ src/build.rs
@@ -0,0 +1,10 @@
+//! Build script to generate the rust code for the rooms. 
+
+use std::io;
+
+fn main() -> io::Result<()> {
+    // TODO: generate rust code for rooms from JSON. 
+
+    // If no errors, return OK. 
+    return Ok(());
+}
