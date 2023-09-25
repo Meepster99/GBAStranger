378962dbd4abba9cd31ea18ff7dd5e07aec3498b
Venus Xeon-Blonde <venusflameblonde@gmail.com>
2023-09-25 01:54:57 -0400

    Add link note

--- src/build.rs
+++ src/build.rs
@@ -4,6 +4,7 @@ use std::io;
 
 fn main() -> io::Result<()> {
     // TODO: generate rust code for rooms from JSON. 
+    // SEE https://doc.rust-lang.org/cargo/reference/build-script-examples.html
 
     // If no errors, return OK. 
     return Ok(());
