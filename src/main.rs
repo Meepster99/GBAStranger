f1aee9a1f9e4d5bee773065cfbf423c76f395858
Venus Xeon-Blonde <venusflameblonde@gmail.com>
2023-09-25 01:31:47 -0400

    Start rust version

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+// Games made using `agb` are no_std which means you don't have access to the standard
+// rust library. This is because the game boy advance doesn't really have an operating
+// system, so most of the content of the standard library doesn't apply.
+//
+// Provided you haven't disabled it, agb does provide an allocator, so it is possible
+// to use both the `core` and the `alloc` built in crates.
+#![no_std]
+// `agb` defines its own `main` function, so you must declare your game's main function
+// using the #[agb::entry] proc macro. Failing to do so will cause failure in linking
+// which won't be a particularly clear error message.
+#![no_main]
+// This is required to allow writing tests
+#![cfg_attr(test, feature(custom_test_frameworks))]
+#![cfg_attr(test, reexport_test_harness_main = "test_main")]
+#![cfg_attr(test, test_runner(agb::test_runner::test_runner))]
+
+// The main function must take 1 arguments and never return. The agb::entry decorator
+// ensures that everything is in order. `agb` will call this after setting up the stack
+// and interrupt handlers correctly. It will also handle creating the `Gba` struct for you.
+#[agb::entry]
+fn main(mut gba: agb::Gba) -> ! {
+    let mut counter = 0;
+    loop {
+        agb::println!("Test {counter}");
+        counter += 1;
+    }
+    // agb::no_game(gba);
+}
