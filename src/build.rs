e08b01ffbf9d937fa39b9ca9127dd3742b6fad8c
Venus Xeon-Blonde <venusflameblonde@gmail.com>
2023-09-28 02:06:46 -0400

    Refactor build script and add layers, game objects

--- src/build.rs
+++ src/build.rs
@@ -12,18 +12,102 @@ struct Room {
     /// The name of the room. Usually matches the file name. 
     name: &'static str,
 
+    /// Width of the room.
+    width: u32,
+
+    /// Height of the room.
+    height: u32,
+
     /// Name of gml file called when room is created. 
     creation_code_id: &'static str,
+
+    /// List of layers associated with this room. 
+    layers: &'static [Layer],
+
+    /// List of game objects in this room.
+    game_objects: &'static [GameObject],
+}
+
+/// A layer defined in a [Room]. 
+struct Layer {
+    /// The layer name.
+    layer_name: &'static str, 
+    /// Is this layer visible?
+    is_visible: bool,
+}
+
+/// A game object used in a [Room].
+struct GameObject {
+    /// The x position of this game object. 
+    x: i32, 
+
+    /// The y position of this game object.
+    y: i32, 
+
+    /// The definition of this game object. 
+    object_definition: &'static str,
 }
 
 "#;
 
 // Deserializable version of all structs put here for use with serde.
+
 #[derive(Deserialize)]
 struct Room {
     name: String, 
+    width: u32,
+    height: u32,
     creation_code_id: String,
+    layers: Vec<Layer>,
+    game_objects: Vec<GameObject>
 }
+
+impl Room {
+    /// Format this room as a rust constant. 
+    fn rust_const(&self) -> String {
+        // Make a comma seperated string of all the layers. 
+        let layers = self.layers.iter()
+            .map(Layer::rust_const)
+            .collect::<Vec<_>>()
+            .join(",\n\t\t");
+
+        // Make a comma seperated list of all the game objects.
+        let game_objects = self.game_objects.iter()
+            .map(GameObject::rust_const)
+            .collect::<Vec<_>>()
+            .join(",\n\t\t");
+
+        format!("Room {{\n\tname: \"{}\", \n\twidth: {}, \n\theight: {}, \n\tcreation_code_id: \"{}\", \n\tlayers: &[\n\t\t{}\n\t], \n\tgame_objects: &[\n\t\t{}\n\t]\n}}", 
+            self.name, 
+            self.width,
+            self.height,
+            self.creation_code_id,
+            layers,
+            game_objects
+        )
+    }
+}
+
+#[derive(Deserialize)]
+struct Layer { layer_name: String, is_visible: bool }
+
+impl Layer {
+    /// Format this layer as a rust constant. 
+    fn rust_const(&self) -> String {
+        format!("Layer {{ layer_name: \"{}\", is_visible: {} }}", self.layer_name, self.is_visible)
+    }
+}
+
+#[derive(Deserialize)]
+struct GameObject { x: i32, y: i32, object_definition: String }
+
+impl GameObject {
+    /// Format this game object as a rust constant. 
+    fn rust_const(&self) -> String {
+        format!("GameObject {{ x: {}, y: {}, object_definition: \"{}\" }}", self.x, self.y, self.object_definition)
+    }
+}
+
 
 fn main() -> io::Result<()> {
     // Re-run if any assets change
@@ -57,11 +141,8 @@ fn main() -> io::Result<()> {
         // Write a const to the end of the rooms file.
         // Use `#[allow(dead_code)]` to spress warnings. 
         writeln!(&mut out_file, "#[allow(dead_code)]")?;
-        writeln!(&mut out_file, r#"const {const_name}: Room = Room {{ name: "{}", creation_code_id: "{}" }};"#, room.name, room.creation_code_id)?;
-        
-
-        // Log message visible to cargo build -vv
-        eprintln!("translated {} to a rust constant.", path.display());
+        // Write room constant.
+        writeln!(&mut out_file, "const {const_name}: Room = {};\n", room.rust_const())?;
     }
 
     // Add all rooms constant.
